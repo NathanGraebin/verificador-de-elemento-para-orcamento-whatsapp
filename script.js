2743608ef2a481dc890b91398ceff4d01144abb6
Nathan <nathangraebin17@gmail.com>
2024-09-30 23:25:32 -0300

    1.5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+var produtos_para_mensagem = "";
+var mensagem = "";
 console.log("0- INICIA");
 
 const produtosX = {
@@ -39,30 +40,70 @@ const produtos_nome = document.querySelectorAll(".product-cart-info .product-nam
 
 const produtos_sku = document.querySelectorAll(".product-cart-info .product-cart-sku");
 
+const produtos_qtd = document.querySelectorAll(".product-cart-actions .input-text.qty");
 
-//const produtos_preco = document.querySelectorAll(".product-cart-info .product-name");
+
+const produtos_preco = document.querySelectorAll(".product-cart-price .cart-price");
 
 
-//const produtos_total = document.querySelectorAll(".product-cart-info .product-name");
+const produtos_total = document.querySelectorAll(".product-cart-total .cart-price");
 
 
 
 
 console.log("1 - CAPTURADOS: ", produtos_nome, produtos_nome.length);
 console.log("1 - CAPTURADOS: ", produtos_sku, produtos_sku.length);
-
-console.log("1.0 - ARRAY 0: ", produtos_nome[0].querySelector("a").textContent);
-console.log("1.0 - ARRAY 0: ", produtos_sku[0].textContent.trim().replace('SKU:', '').trim());
-console.log("1.1 - ARRAY 1: ", produtos_nome[1].querySelector("a").textContent);
-console.log("1.1 - ARRAY 1: ", produtos_sku[1].textContent.trim().replace('SKU:', '').trim());
-console.log("1.2 - ARRAY 2: ", produtos_nome[2].querySelector("a").textContent);
-console.log("1.2 - ARRAY 2: ", produtos_sku[2].textContent.trim().replace('SKU:', '').trim());
+console.log("1 - CAPTURADOS: ", produtos_qtd, produtos_qtd.length);
 
 console.log("----------------------------------------");
 
+console.log("1.0 - ARRAY 0 - NOME> ", produtos_nome[0].querySelector("a").textContent);
+console.log("1.0 - ARRAY 0 - SKU> ", produtos_sku[0].textContent.replace('SKU:', '').trim());
+console.log("1.0 - ARRAY 0 - QTD> ", produtos_qtd[0].value);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO>", produtos_preco[0].querySelector("span").textContent);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO S/ R$>", produtos_preco[0].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+console.log("1.0 - ARRAY 0: TOTAL>", produtos_total[0].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+
+
+console.log("----------------------------------------");
+
+console.log("1.0 - ARRAY 0 - NOME> ", produtos_nome[1].querySelector("a").textContent);
+console.log("1.0 - ARRAY 0 - SKU> ", produtos_sku[1].textContent.replace('SKU:', '').trim());
+console.log("1.0 - ARRAY 0 - QTD> ", produtos_qtd[1].value);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO>", produtos_preco[1].querySelector("span").textContent);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO S/ R$>", produtos_preco[1].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+console.log("1.0 - ARRAY 0: TOTAL>", produtos_total[1].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+
+console.log("----------------------------------------");
+
+console.log("1.0 - ARRAY 0 - NOME> ", produtos_nome[2].querySelector("a").textContent);
+console.log("1.0 - ARRAY 0 - SKU> ", produtos_sku[2].textContent.replace('SKU:', '').trim());
+console.log("1.0 - ARRAY 0 - QTD> ", produtos_qtd[2].value);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO>", produtos_preco[2].querySelector("span").textContent);
+console.log("1.0 - ARRAY 0: PREÇO PRODUTO S/ R$>", produtos_preco[2].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+console.log("1.0 - ARRAY 0: TOTAL>", produtos_total[2].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+console.log("----------------------------------------");
+
+for(let i = 0; i < produtos_nome.length; i++) {
+
+    console.log("################################################################################################");
+    console.log("1.0 - ARRAY 0 - NOME> ", produtos_nome[i].querySelector("a").textContent);
+    console.log("1.0 - ARRAY 0 - SKU> ", produtos_sku[i].textContent.replace('SKU:', '').trim());
+    console.log("1.0 - ARRAY 0 - QTD> ", produtos_qtd[i].value);
+    console.log("1.0 - ARRAY 0: PREÇO PRODUTO>", produtos_preco[i].querySelector("span").textContent);
+    console.log("1.0 - ARRAY 0: PREÇO PRODUTO S/ R$>", produtos_preco[i].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+    console.log("1.0 - ARRAY 0: TOTAL>", produtos_total[i].querySelector("span").textContent.replace(/^R\$?\s*/, '').trim());
+
+    mensagem = mensagem +1;
+    
+    
+}
+console.log(mensagem);
+
 console.log("2 - INICI FOREACH");
 i = 0;
-var produtos_para_mensagem = "1";
+
+produtos_para_mensagem
 produtos_nome.forEach(produtos => {
 
     console.log("2.1 - DENTRO DO FOREACH");
